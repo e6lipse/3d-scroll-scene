3964b97a0375a7299ac0efbaf57029bd906c966b
Nazarii Kolomiiets <90979059+e6lipse@users.noreply.github.com>
2022-11-12 20:52:54 +0200

    Add files via upload

---
+++ 3d-scroll-scene/assets/js/app-audio.js
@@ -0,0 +1,16 @@
+// Audio
+let soundButton = document.querySelector('.soundbutton');
+let audio = document.querySelector('.audio');
+
+soundButton.addEventListener('click', (e) => {
+  soundButton.classList.toggle('paused');
+  audio.paused ? audio.play() : audio.pause();
+});
+
+window.onfocus = function () {
+  soundButton.classList.contains('paused') ? audio.pause() : audio.play();
+};
+
+window.onblur = function () {
+  audio.pause();
+};
